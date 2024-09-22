98fbea3b8101a2c8cf26dbb0210bae8ddc39e931
Dk2698 <dileepkumar1823@gmail.com>
2024-09-22 12:59:44 +0530

    update chainResponsibility pattern

---
+++ src/com/kumar/chainresponsibility/InfoLogProcessor.java
@@ -0,0 +1,19 @@
+package com.kumar.chainresponsibility;
+
+/**
+ * Log processor for handling INFO level logs.
+ */
+public class InfoLogProcessor extends LogProcessor {
+    InfoLogProcessor(LogProcessor loggerProcessor) {
+        super(loggerProcessor);
+    }
+
+    @Override
+    public void log(int logLevel, String message) {
+        if (logLevel == INFO) {
+            System.out.println("INFO: " + message);
+        } else {
+            super.log(logLevel, message); // Pass to the next processor
+        }
+    }
+}
---
+++ src/com/kumar/chainresponsibility/LogProcessor.java
@@ -0,0 +1,33 @@
+package com.kumar.chainresponsibility;
+
+/**
+ * Abstract class representing a log processor in the chain of responsibility.
+ */
+public abstract class LogProcessor {
+    public static final int INFO = 1;
+    public static final int DEBUG = 2;
+    public static final int ERROR = 3;
+
+    protected LogProcessor nextLoggerProcessor;
+
+    /**
+     * Constructor to set the next logger in the chain.
+     *
+     * @param loggerProcessor the next log processor in the chain
+     */
+    LogProcessor(LogProcessor loggerProcessor) {
+        this.nextLoggerProcessor = loggerProcessor;
+    }
+
+    /**
+     * Method to log messages.
+     *
+     * @param logLevel the severity level of the log
+     * @param message the message to log
+     */
+    public void log(int logLevel, String message) {
+        if (nextLoggerProcessor != null) {
+            nextLoggerProcessor.log(logLevel, message);
+        }
+    }
+}
