428e178a13451455fcff4f248bb9e781bbf2316c
Dk2698 <dileepkumar1823@gmail.com>
2024-09-27 08:45:26 +0530

    state pattern

---
+++ src/com/kumar/state/OffState.java
@@ -0,0 +1,24 @@
+package com.kumar.state;
+
+/**
+ * Represents the Off state of the phone.
+ */
+public class OffState extends State {
+    public OffState(Phone phone) {
+        super(phone);
+    }
+
+    @Override
+    public String onHome() {
+        // Transition to Locked state when home button is pressed
+        phone.setState(new LockedState(phone));
+        return phone.turnOn();
+    }
+
+    @Override
+    public String onOffOn() {
+        // Remain in Locked state, turn the screen on
+        phone.setState(new LockedState(phone));
+        return phone.turnOn();
+    }
+}
